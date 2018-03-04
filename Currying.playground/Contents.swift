0e4b1f1b1b4c9235be03aef872b1d4df43468e1b
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-04 11:33:16 +0100

    Working with imperative programming connectors

--- Currying.playground/Contents.swift
+++ Currying.playground/Contents.swift
@@ -1,1 +1,15 @@
 //: Playground - Currying
+
+class Connector {
+    func connect(host: String, port: Int) {
+        //connection logic
+    }
+    
+    func connect(host: String) {
+        connect(host: host, port: 80)
+    }
+    
+    func connect()  {
+        connect(host: "localhost")
+    }
+}
