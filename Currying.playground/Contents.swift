67763a7d82197537a20201107e1d4c69bab16951
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-04 11:42:42 +0100

    Builder pattern to build Conectors

--- Currying.playground/Contents.swift
+++ Currying.playground/Contents.swift
@@ -1,17 +1,43 @@
 //: Playground - Currying
 
-func connect(host: String) -> (Int) -> Void {
-    return { port in
+class Connector {
+    let host: String?
+    let port: Int?
+    
+    init(host: String?, port: Int?) {
+        self.host = host
+        self.port = port
+    }
+    
+    func connect() {
         //connection logic
     }
-
 }
 
+class ConnectorBuilder {
+    var connector = Connector(host: nil, port: nil)
+    
+    func set(host: String) -> Self {
+        connector = Connector(host: host, port: connector.port)
+        
+        return self
+    }
+    
+    func set(port: Int) -> Self {
+        connector = Connector(host: connector.host, port: port)
+        
+        return self
+    }
+    
+    func build() -> Connector {
+        return connector
+    }
+}
 
-connect(host: "localhost")(80)
+ConnectorBuilder()
+    .set(host: "localhost")
+    .set(port: 80)
+    .build()
+    .connect()
 
-let localConnector = connect(host: "localhost")
-
-localConnector(80)
-localConnector(8888)
 
