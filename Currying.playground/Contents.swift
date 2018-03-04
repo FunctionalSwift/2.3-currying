9499a7afaa011ccc15b98c23aa007820183b60fc
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-04 11:39:40 +0100

    This is it! Currying!!

--- Currying.playground/Contents.swift
+++ Currying.playground/Contents.swift
@@ -1,6 +1,17 @@
 //: Playground - Currying
 
-func connect(host: String = "localhost", port: Int = 80) {
-    //connection logic
+func connect(host: String) -> (Int) -> Void {
+    return { port in
+        //connection logic
+    }
+
 }
+
+
+connect(host: "localhost")(80)
+
+let localConnector = connect(host: "localhost")
+
+localConnector(80)
+localConnector(8888)
 
