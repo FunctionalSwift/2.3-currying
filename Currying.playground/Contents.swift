740483b61a77c461f18257f830e658f8f2e1c8f8
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-04 11:46:16 +0100

    mmmm.. We have a meaningless operation.. What's happened?

--- Currying.playground/Contents.swift
+++ Currying.playground/Contents.swift
@@ -4,3 +4,18 @@ func consoleLogger(type: String, msg: String) {
     print("[\(type)]    \(msg)")
 }
 
+func operation(a: Int, b: Int, logger: (String, String) -> ()) {
+    guard a > 0 else {
+        logger("ERROR", "a must be positive")
+        return
+    }
+    
+    guard b > 0 else {
+        logger("ERROR", "b must be positive")
+        return
+    }
+    
+    //do something with a and b
+}
+
+operation(a: 1, b: -2, logger: consoleLogger)
