efafd3dcb034da0ab064a0180366091fd63f8186
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-04 11:55:53 +0100

    This is magic, Currying again!!

--- Currying.playground/Contents.swift
+++ Currying.playground/Contents.swift
@@ -1,21 +1,23 @@
 //: Playground - Currying
 
-func consoleLogger(type: String, msg: String) {
-    print("[\(type)]    \(msg)")
+func consoleLogger(type: String) -> (String) -> Void {
+    return { msg in
+        print("[\(type)]    \(msg)")
+    }
 }
 
-func operation(a: Int, b: Int, logger: (String, String) -> ()) {
+func operation(a: Int, b: Int, logger: (String) -> ()) {
     guard a > 0 else {
-        logger("ERROR", "a must be positive")
+        logger("a must be positive")
         return
     }
     
     guard b > 0 else {
-        logger("ERROR", "b must be positive")
+        logger("b must be positive")
         return
     }
     
     //do something with a and b
 }
 
-operation(a: 1, b: -2, logger: consoleLogger)
+operation(a: 1, b: 2, logger: consoleLogger(type: "ERROR"))
